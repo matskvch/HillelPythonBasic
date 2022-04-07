947c9a633e8bd352171fad0041eee4f65bf55841
matskvch <matskevych.yura@gmail.com>
2022-04-07 14:07:32 +0300

    Homework_03 corrected 1

--- Homework_03/homework_06.py
+++ Homework_03/homework_06.py
@@ -1,7 +1,6 @@
 klass_1 = int(input('Введите кол-во учеников в классе 1: '))
 klass_2 = int(input('Введите кол-во учеников в классе 2: '))
 klass_3 = int(input('Введите кол-во учеников в классе 3: '))
-summ = klass_1 + klass_2 + klass_3
-tables = summ // 2
-add_tables = summ % 2
-print('Нужно купить', tables + add_tables, 'парт')
+tables = klass_1 // 2 + klass_2 // 2 + klass_3 // 2 + klass_1 % 2 + klass_2 % 2 + klass_3 % 2
+print('Всего учеников', klass_1 + klass_2 + klass_3)
+print('Нужно купить', tables, 'парт')
